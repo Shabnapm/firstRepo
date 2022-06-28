7fe82b1987bea817527e077cd1ea78d7ed403a67
Shabnapm <shabna.pm@capgemini.com>
2022-06-28 18:23:14 +0530

    Introduce a Dynamic Attribute

---
+++ custom/concerttours/src/concerttours/attributehandlers/ConcertDaysUntilAttributeHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package concerttours.attributehandlers;
+
+import de.hybris.platform.servicelayer.model.attribute.AbstractDynamicAttributeHandler;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.springframework.stereotype.Component;
+
+import concerttours.model.ConcertModel;
+
+
+@Component
+public class ConcertDaysUntilAttributeHandler extends AbstractDynamicAttributeHandler<Long, ConcertModel>
+{
+	@Override
+	public Long get(final ConcertModel model)
+	{
+		if (model.getDate() == null)
+		{
+			return null;
+		}
+		final ZonedDateTime concertDate = model.getDate().toInstant().atZone(ZoneId.systemDefault());
+		final ZonedDateTime now = ZonedDateTime.now();
+		if (concertDate.isBefore(now))
+		{
+			return Long.valueOf(0L);
+		}
+		final Duration duration = Duration.between(now, concertDate);
+		return Long.valueOf(duration.toDays());
+    }
+}
